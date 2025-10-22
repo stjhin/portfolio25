208f078da65c8f83e237b3c00e4a59dd00117709
stjhin <stjhin@gmail.com>
2025-10-22 11:21:24 -0400

    add wrap-root-element.js

--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-import { wrapRootElement as minimalBlogWrapRootElement } from "@lekoarts/gatsby-theme-minimal-blog/src/wrap-root-element"
-
-export const wrapRootElement = minimalBlogWrapRootElement
