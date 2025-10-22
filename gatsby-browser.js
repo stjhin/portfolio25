cfcfa8b838868655759d37126caaea7259b07b7a
stjhin <stjhin@gmail.com>
2025-10-22 11:15:38 -0400

    Final FIX: Cleaned up gatsby-browser.js

---
+++ gatsby-browser.js
@@ -0,0 +1,3 @@
+import { wrapRootElement as minimalBlogWrapRootElement } from "@lekoarts/gatsby-theme-minimal-blog/src/wrap-root-element"
+
+export const wrapRootElement = minimalBlogWrapRootElement
